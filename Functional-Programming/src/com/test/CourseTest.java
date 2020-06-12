62bb33ff1c9ad5baa17563a4bf0cd9eea13c3f4c
creationk <creationk@users.noreply.github.com>
2020-06-12 21:30:37 +0530

    Moving everything to a single package

--- Functional-Programming/src/com/test/CourseTest.java
+++ Functional-Programming/src/com/test/CourseTest.java
@@ -10,8 +10,6 @@ import java.util.stream.Collectors;
 
 import org.junit.Test;
 
-import com.app.Course;
-
 public class CourseTest {
 	private static List<Course> courses;
 	private static Predicate<Course> scoreGT92 = course -> course.getReviewScore() > 92;
@@ -118,9 +116,61 @@ public class CourseTest {
 	public void groupCoursesByCategory() {
 		System.out.println("\n:::::::::::::: groupCoursesByCategory ::::::::::::::");
 		System.out.println(courses.stream().collect(Collectors.groupingBy(Course::getCategory)));
-	
+
 		System.out.println(courses.stream().collect(Collectors.groupingBy(Course::getCategory,
 				Collectors.maxBy(Comparator.comparing(Course::getNoOfStudents)))));
+	}
+
+}
+
+class Course {
+	private String name;
+	private String category;
+	private int reviewScore;
+	private int noOfStudents;
+
+	public Course(String name, String category, int reviewScore, int noOfStudents) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.reviewScore = reviewScore;
+		this.noOfStudents = noOfStudents;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getReviewScore() {
+		return reviewScore;
+	}
+
+	public void setReviewScore(int reviewScore) {
+		this.reviewScore = reviewScore;
+	}
+
+	public int getNoOfStudents() {
+		return noOfStudents;
+	}
+
+	public void setNoOfStudents(int noOfStudents) {
+		this.noOfStudents = noOfStudents;
+	}
+
+	public String toString() {
+		return name + ":" + category + ":" + reviewScore + ":" + noOfStudents;
 	}
 
 }
